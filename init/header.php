6b8c15f19680de3fc713e9b85a02029e7ed16e6e
Breksam Elsokkary <76849788+Breksam@users.noreply.github.com>
2023-03-22 01:46:23 +0200

    Add files via upload

---
+++ init/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titlePage;?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+    <a class="navbar-brand" href="../Home/index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="../Users/users.php">Users</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../Products/products.php">Products</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../Categories/categories.php">Categories</a>
+        </li>
+        </ul>
+    </div>
+    </div>
+</nav>
